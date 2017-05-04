a82a303ba762cf13797e0c742479a48a7cb6eff1
jprecreativo <jprecreativo@gmail.com>
2017-05-04 19:31:48 +0200

    - Sistema de puntos implementado.

--- Assets/Scripts/DestruyeBloque.cs
+++ Assets/Scripts/DestruyeBloque.cs
@@ -3,10 +3,12 @@
 public class DestruyeBloque : MonoBehaviour
 {
     public GameObject particleSys;
+    public ModificarPuntos puntos;
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
         Instantiate(particleSys, this.transform.position, Quaternion.identity);
+        puntos.ganarPuntos();
     }
 }
---
+++ Assets/Scripts/ModificarPuntos.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ModificarPuntos : MonoBehaviour
+{
+    public static int puntos;
+    public Text textoPuntos;
+
+    private void actualizarTextoPuntos()
+    {
+        textoPuntos.text = "Puntos: " + ModificarPuntos.puntos;
+    }
+
+    public void Start ()
+    {
+        puntos = 0;
+        this.actualizarTextoPuntos();
+	}
+
+    public void ganarPuntos()
+    {
+        ModificarPuntos.puntos++;
+
+        this.actualizarTextoPuntos();
+    }
+}
